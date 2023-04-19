a054e8c26ce1b6dc9ff6a781a87937172036efac
Blake Erickson <o.blakeerickson@gmail.com>
2023-04-19 06:18:04 -0600

    posts update and likes

--- lib/discourse_api_curl/post.rb
+++ lib/discourse_api_curl/post.rb
@@ -36,5 +36,32 @@ module DiscourseApiCurl
       request = command.delete("/posts/#{id}.json", params)
       command.exec(request)
     end
+
+    def self.update(command, id, args = {})
+      params = DiscourseApiCurl.params(args)
+        .required(:raw)
+        .optional(:edit_reason)
+
+      h = {}
+      params.to_h.each do |k,v|
+        h["post[#{k}]"] = v
+      end
+
+      request = command.put("/posts/#{id}.json", h)
+      command.exec(request)
+    end
+
+    def self.like(command, args)
+      params = DiscourseApiCurl.params(args)
+        .required(:id, :post_action_type_id)
+        .default(flag_topic: false)
+      request = command.post("/post_actions.json", params)
+      command.exec(request)
+    end
+
+    def self.likes(command, args)
+      request = command.get("/posts/#{id}.json")
+      command.exec(request)
+    end
   end
 end
